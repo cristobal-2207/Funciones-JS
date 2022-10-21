0231c8301f3417047f18a8142af09129918e4064
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-10-21 10:08:50 -0300

    4to Commit: Se realiza ejercicio 3 del Proyecto.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-function pintar(){
+/* Modifica el código anterior para poder pasarle un color como argumento a la función pintar. El color debe ser verde (green) por defecto, al hacer clic en el párrafo se debe pasar amarillo como color. (1 Punto).
+
+function pintar("green"){
   ele = document.getElementById("ele1")
   ele.style.backgroundColor = 'yellow'
-  }
+}
 ele = document.getElementById("ele1")
 ele.addEventListener("click", pintar);
+*/
+
+const ele = document.getElementById("ele1");
+ele.style.backgroundColor = "green";
+
+ele.addEventListener('click', () => {
+  ele.style.backgroundColor = "yellow";
+})
